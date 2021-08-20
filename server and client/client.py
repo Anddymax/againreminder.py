44c124955c99b6f71fd52c5ebb18a54621b945b6
Aniket shisode <70856755+Anddymax@users.noreply.github.com>
2021-08-20 16:04:42 +0530

    Add files via upload
    creating server and connecting client to it

---
+++ server and client/client.py
@@ -0,0 +1,20 @@
+#255.255.255.0
+import socket
+import threading
+PORT=5050
+SERVER=socket.gethostbyname(socket.gethostname())
+ADDR=(SERVER, PORT)
+
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+def handle_client(conn, addr):
+    print(f"NEW CONNECTION {addr} connected.")
+def start():
+    server.listen()
+    while True:
+        conn, addr=server.accept()
+        thread = threading.Thread(target=handle_client,args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threating.activeCount()-1} ")
+print("[SRARTING] server is starting.....")
+start()
